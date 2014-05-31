8c3892f9ea468c2d062d12afe7e045e43fc35c5f
Martin Malmsten <martin.malmsten@gmail.com>
2014-05-31 17:20:26 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='starch',
+        version='0.0.1',
+        description='Semantic Tiny ARCHive',
+        author='Martin Malmsten',
+        author_email='martin@martinmalmsten.net',
+        packages=[ 'starch' ])

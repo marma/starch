c13e3aeaabaa934097f4e3ff6ed8224d1eb974da
Martin Malmsten <martin.malmsten@gmail.com>
2014-06-06 12:49:31 +0200

    minor edits

--- starch/archive.py
+++ starch/archive.py
@@ -3,66 +3,74 @@
 from rdflib import Graph,Namespace,URIRef,RDF
 from memorystore import MemoryStore
 from diskstore import DiskStore
+from package import Package
 from sys import argv
-from utils import get_property
+from utils import get_property,url_pather
+import logging
+
+DCTERMS = Namespace('http://purl.org/dc/terms/')
 
 class Archive:
     def __init__(self, store=MemoryStore(), graph=Graph(), vocab=Namespace('http://example.org/vocab#')):
         self.store = store
         self.graph = graph
-        self.VOCAB = vocab
-        self.DCTERMS = Namespace('http://purl.org/dc/terms/')
+        self.vocab = vocab
 
-    def store(self, url=None, data=None, uri=None, properties=None):
+    def __store(self, url=None, data=None, uri=None, properties={}):
         assert url or data
         return self.store.store(url=url, data=data, uri=uri, properties=properties)
 
     def ingest(self, url):
-        g = Graph()
-        g.parse(url, format='turtle')
+        p = Package(url, vocab=self.vocab)
 
-        # find :Package and iterate over :Resources :contained within
-        for package in g.subjects(RDF.type, self.VOCAB.Package):
-            print package
-
-            for resource in g.objects(package, self.VOCAB.contains):
-                prop = {}
-                uri = get_property(g, resource, self.VOCAB.uri)
-                format = get_property(g, resource, URIRef('http://purl.org/dc/terms/format'))
-
-                if format:
-                    prop['content-type'] = format
-
-                self.store.store(url=resource, uri=uri, properties=prop)
+        for res in p:
+            mime = p.get(res, str(DCTERMS.term('format')))
+            self.__store(res, uri=p.get(res, str(self.vocab.uri)), properties={ 'content-type': mime } if mime else {})
 
 
-        """
-        # find all Resources in package and store them
-        for resource in g.subjects(RDF.type, self.VOCAB.Resource):
-            uri=None
-            for o in g.objects(resource, self.VOCAB.uri):
-                uri=o
-
-            describedByUri=None
-            for o in g.objects(resource, self.VOCAB.describedByUri):
-                describedByUri=o
-
-            prop = {}
-            for s,p,o in g.triples((resource, self.DCTERMS.format, None)):
-                prop['content-type'] = str(o)
-                print prop
-
-            desc = Graph()
-            for s,p,o in g.triples((resource, None, None)):
-                desc.add((uri,p,o))
-
-            self.store.store(url=str(resource), uri=str(uri))
-            self.store.store(data=desc.serialize(format='turtle'), uri=str(describedByUri), properties=prop)
-        """
+#        # find :Package and iterate over :Resources :contained within
+#        for package in g.subjects(RDF.type, self.VOCAB.Package):
+#            print package
+#
+#            for resource in g.objects(package, self.VOCAB.contains):
+#                prop = {}
+#                uri = get_property(g, resource, self.VOCAB.uri)
+#                format = get_property(g, resource, URIRef('http://purl.org/dc/terms/format'))
+#
+#                if format:
+#                    prop['content-type'] = format
+#
+#                self.store.store(url=resource, uri=uri, properties=prop)
+#
+#
+#        """
+#        # find all Resources in package and store them
+#        for resource in g.subjects(RDF.type, self.VOCAB.Resource):
+#            uri=None
+#            for o in g.objects(resource, self.VOCAB.uri):
+#                uri=o
+#
+#            describedByUri=None
+#            for o in g.objects(resource, self.VOCAB.describedByUri):
+#                describedByUri=o
+#
+#            prop = {}
+#            for s,p,o in g.triples((resource, self.DCTERMS.format, None)):
+#                prop['content-type'] = str(o)
+#                print prop
+#
+#            desc = Graph()
+#            for s,p,o in g.triples((resource, None, None)):
+#                desc.add((uri,p,o))
+#
+#            self.store.store(url=str(resource), uri=str(uri))
+#            self.store.store(data=desc.serialize(format='turtle'), uri=str(describedByUri), properties=prop)
+#        """
 if __name__ == "__main__":
     if len(argv) != 3:
         print "usage: %s <base> <package>" % argv[0]
         exit(1)
 
-    archive = Archive(store=DiskStore(argv[1]))
+    logging.basicConfig()
+    archive = Archive(store=DiskStore(argv[1], pather=url_pather))
     archive.ingest(argv[2])
--- starch/utils.py
+++ starch/utils.py
@@ -5,10 +5,11 @@ from hashlib import md5
 from re import split
 from contextlib import contextmanager
 from os import makedirs
-from os.path import dirname,exists
+from os.path import dirname,exists,abspath
 from hashlib import sha1
 from io import BytesIO
 from urllib2 import urlopen
+from random import random
 
 def md5_pather(uri):
     hex = md5(uri).hexdigest()
@@ -17,12 +18,15 @@ def md5_pather(uri):
 
 def url_pather(uri):
     if uri[0:8] == 'urn:uuid':
-        s = [ 'urn', 'uuid' ] + [ s[8:][ 2*i:2*i+2 ] for i in range(0,4) ] + [ s[8:] ]
+        s = [ 'urn', 'uuid' ] + [ uri[9:][ 2*i:2*i+2 ] for i in range(0,4) ] + [ uri ]
     elif uri[0:7] == 'urn:nbn':
         hex = md5(uri).hexdigest()
         s = [ 'urn', 'nbn' ] + [ hex[ 2*i:2*i+2 ] for i in range(0,4) ] + [ hex ]
     elif uri[0:4] == 'http':
         s = [ x for x in split(':|/', uri) if x != '' ]
+    else:
+        hex = md5(uri).hexdigest()
+        s = [ 'md5' ] + [ hex[ 2*i:2*i+2 ] for i in range(0,4) ] + [ hex ]
 
     if uri[-1] == '/':
         return [ '/'.join(s), '.content' ]
@@ -71,3 +75,25 @@ def get_property(g, s, p, default=None):
         return str(ret)
 
     return default or None
+
+
+def normalize_url(url):
+    if url.find(':') != -1:
+        if url.split(':')[0] not in [ 'file', 'http', 'https', 'ftp' ]:
+            raise Exception('unsupported protocol')
+
+        if 'file:///' in url:
+            return url
+        elif 'file:' in url:
+            return 'file://' + abspath(url[5:])
+        else:
+            return url
+    else:
+        return 'file://' + abspath(url)
+
+
+def random_slug():
+    return sha1(str(random())).hexdigest()
+
+
+

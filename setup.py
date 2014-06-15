d2ee951747b3d5b3f3543884fe8f81bf8266a069
Martin Malmsten <martin.malmsten@gmail.com>
2014-06-15 23:29:49 +0200

    changes

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
-setup(name='starch',
-        version='0.0.1',
-        description='Semantic Tiny ARCHive',
-        author='Martin Malmsten',
-        author_email='martin@martinmalmsten.net',
-        packages=[ 'starch' ])
+setup(name='starch', version='0.0.1', description='Semantic Tiny ARCHive', author='Martin Malmsten', author_email='martin@martinmalmsten.net', packages=[ 'starch' ])
